ccc62fb08751baaba761e216fe567b17d8cf64f6
Brian Douglas Smith <m.b1205@gmail.com>
2012-03-02 22:54:19 -0500

    Remove unused VCR options

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -1,11 +1,8 @@
 require 'vcr'
 
-uri_without_auth_token  = VCR.request_matchers.uri_without_param(:auth_token)
-
 VCR.configure do |c|
   c.cassette_library_dir = 'vcr_cassettes'
   c.hook_into :webmock
-  c.default_cassette_options = { :match_requests_on => [:method, uri_without_auth_token] }
 end
 
 RSpec.configure do |c|
